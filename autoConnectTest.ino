f65f518234bd3eccbac661a86c78f446f825b3ae
Michał Bronowski <michal.bronowski@hxs.pl>
2023-07-19 12:36:38 +0200

    set pointers to relays and box

--- autoConnectTest.ino
+++ autoConnectTest.ino
@@ -4,6 +4,8 @@
 #include <LittleFS.h>
 #include <ESP8266WiFi.h>
 #include <FS.h>
+#include <uri/UriBraces.h>
+#include <uri/UriRegex.h>
 
 #include "Relay.h"
 #include "Box.h"
@@ -18,12 +20,13 @@ ESP8266WebServer server(80);
 WiFiManager wifiManager;
 
 // Box box(1, "timer", D1, D3);
-Box Boxes[1] = { Box("1", "Box 1", D1, D3) };
+Box Boxes[1] = {Box("1", "box-1", D1, D3)};
 
 const IPAddress staticIP(192, 168, 55, 126);
 const IPAddress gatewayIP(192, 168, 55, 1);
 const IPAddress subnetMask(255, 255, 255, 0);
 
+Box *getBoxByName(String name); // convert the file extension to the MIME type
 String getContentType(String filename); // convert the file extension to the MIME type
 bool handleFileRead(String path);       // send the right file to the client (if it exists)
 
@@ -51,6 +54,19 @@ void setup()
     server.on("/devices", handleDevices);
 
     server.on("/pump/on", handlePumpOn);
+
+    server.on(UriBraces("/{}/pump/on"), []() 
+        {
+            String boxName = server.pathArg(0);
+
+            Box *box = getBoxByName(boxName);
+
+            Serial.println("handleFileRead: " + box->getName());
+
+            server.send(200, "text/plain", "Pump turned on"); 
+        }
+    );
+
     server.on("/pump/off", handlePumpOff);
     server.on("/pump/trigger", handlePumpTrigger);
     server.on("/pump/toggle", handlePumpToggle);
@@ -72,6 +88,19 @@ void setup()
 void loop()
 {
     server.handleClient();
+}
+
+Box *getBoxByName(String name)
+{
+    for (unsigned int i = 0; i < sizeof Boxes / sizeof Boxes[0]; i++)
+    {
+        if (Boxes[i].getName() == name)
+        {
+            return &Boxes[i]; // Return a pointer to the matching object
+        }
+    }
+
+    return NULL; // Return NULL if no match is found
 }
 
 void handleNotFound()
@@ -146,10 +175,11 @@ void handleDevices()
 {
     DynamicJsonDocument doc(1024);
 
-    for (unsigned int i=0; i<sizeof Boxes/sizeof Boxes[0]; i++) {
-        Box* box = &Boxes[i];
-        Relay* pump = box->getPump();
-        Relay* light = box->getLight();
+    for (unsigned int i = 0; i < sizeof Boxes / sizeof Boxes[0]; i++)
+    {
+        Box *box = &Boxes[i];
+        Relay *pump = box->getPump();
+        Relay *light = box->getLight();
         doc["devices"][box->getName()]["pump"]["name"] = pump->getName();
         doc["devices"][box->getName()]["pump"]["state"] = pump->getState();
         doc["devices"][box->getName()]["light"]["name"] = light->getName();
@@ -163,8 +193,8 @@ void handleDevices()
 
 void handlePumpOn()
 {
-    Box* box = &Boxes[0];
-    Relay* pump = box->getPump();
+    Box *box = &Boxes[0];
+    Relay *pump = box->getPump();
 
     Serial.println(pump->getName());
     pump->turnON();
@@ -173,7 +203,7 @@ void handlePumpOn()
 
 void handlePumpOff()
 {
-    Box* box = &Boxes[0];
+    Box *box = &Boxes[0];
     Serial.println(box->getPump()->getName());
     box->getPump()->turnOFF();
     server.send(200, "text/plain", box->getPump()->getStateJson());
@@ -181,7 +211,7 @@ void handlePumpOff()
 
 void handlePumpToggle()
 {
-    Box* box = &Boxes[0];
+    Box *box = &Boxes[0];
     Serial.println(box->getPump()->getName());
     box->getPump()->toggle();
     server.send(200, "text/plain", box->getPump()->getStateJson());
@@ -189,7 +219,7 @@ void handlePumpToggle()
 
 void handlePumpTrigger()
 {
-    Box* box = &Boxes[0];
+    Box *box = &Boxes[0];
     Serial.println(box->getPump()->getName());
     box->getPump()->trigger();
     server.send(200, "text/plain", box->getPump()->getStateJson());
@@ -197,7 +227,7 @@ void handlePumpTrigger()
 
 void handlePlugOn()
 {
-    Box* box = &Boxes[0];
+    Box *box = &Boxes[0];
     Serial.println(box->getLight()->getName());
     box->getLight()->turnON();
     server.send(200, "text/plain", box->getLight()->getStateJson());
@@ -205,7 +235,7 @@ void handlePlugOn()
 
 void handlePlugOff()
 {
-    Box* box = &Boxes[0];
+    Box *box = &Boxes[0];
     Serial.println(box->getLight()->getName());
     box->getLight()->turnOFF();
     server.send(200, "text/plain", box->getLight()->getStateJson());
@@ -213,7 +243,7 @@ void handlePlugOff()
 
 void handlePlugToggle()
 {
-    Box* box = &Boxes[0];
+    Box *box = &Boxes[0];
     Serial.println(box->getLight()->getName());
     box->getLight()->toggle();
     server.send(200, "text/plain", box->getLight()->getStateJson());
